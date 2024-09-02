edd106f89b7e32e514edfc7e6c4e95072313b5fd
Tim Moreton <tmoreton89@gmail.com>
2024-09-02 10:14:48 -0400

    redo video

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,12 +1,8 @@
 import { Text, View } from 'react-native';
 import { useAuth } from '@/providers/AuthProvider';
+import Videos from '@/components/videos';
 
 export default function () {
   const { user } = useAuth();
-  return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-black font-bold text-3xl">Home</Text>
-      <Text>{JSON.stringify(user)}</Text>
-    </View>
-  );
+  return <Videos />
 }
--- app/camera.tsx
+++ app/camera.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
 import { useState } from 'react';
-import { Button, Text, TouchableOpacity, View } from 'react-native';
+import { Button, Text, TouchableOpacity, View, Dimensions } from 'react-native';
 import Ionicons from '@expo/vector-icons/Ionicons';
 import { supabase } from '@/utils/supabase';
 import { useAuth } from '@/providers/AuthProvider';
 import { useRouter } from 'expo-router';
+import * as ImagePicker from 'expo-image-picker';
+import { Video, ResizeMode } from 'expo-av';
 
 export default function App() {
   const [facing, setFacing] = useState<CameraType>('back');
   const [permission, requestPermission] = useCameraPermissions();
   const [isRecording, setIsRecording] = useState(false);
   const cameraRef = React.useRef<CameraView>(null);
+  const videoRef = React.useRef<Video>(null);
   const [videoUri, setVideoUri] = useState<string | null>(null);
   const { user } = useAuth();
   const router = useRouter();
+  const [status, setStatus] = useState({ isLoaded: false, isPlaying: false });
 
   if (!permission) {
     // Camera permissions are still loading.
@@ -56,7 +60,7 @@ export default function App() {
     });
 
     const { data, error } = await supabase.storage
-      .from('videos')
+      .from(`videos/${user?.id}`)
       .upload(fileName, formData, {
         cacheControl: '3600000000',
         upsert: false
@@ -65,19 +69,55 @@ export default function App() {
 
     const { error: videoError } = await supabase.from('Video').insert({
       title: "Test title here!",
-      uri: data.path,
+      uri: `${user?.id}/${fileName}`,
       user_id: user?.id
     });
     if(videoError) console.error(videoError);
     router.back();
   }
 
+  const pickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 0.1,
+    });
+    setVideoUri(result.assets[0].uri);
+  };
+
   return (
+    <View className="flex-1">
+      {videoUri ? (
+        <View className="flex-1">
+          <TouchableOpacity className="absolute bottom-10 left-36 z-10" onPress={saveVideo}>
+            <Ionicons name="checkmark-circle" size={100} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity className="flex-1" onPress={() => status.isPlaying ? videoRef.current.pauseAsync() : videoRef.current.playAsync()}>
+            <Video
+              ref={videoRef}
+              style={{ 
+                flex: 1,
+                width: Dimensions.get('window').width,
+                height: Dimensions.get('window').height
+              }}
+              source={{
+                uri: videoUri
+              }}
+              resizeMode={ResizeMode.COVER}
+              isLooping
+              onPlaybackStatusUpdate={status => setStatus(() => status)}
+            />
+          </TouchableOpacity>
+        </View>
+      )
+      :
       <CameraView mode="video" ref={cameraRef} style={{ flex: 1 }} facing={facing}>
         <View className="flex-1 justify-end">
           <View className="flex-row items-center justify-around mb-10">
-            <TouchableOpacity className="items-end justify-end" onPress={toggleCameraFacing}>
-              <Ionicons name="camera-reverse" size={50} color="transparent" />
+            <TouchableOpacity className="items-end justify-end" onPress={pickImage}>
+              <Ionicons name="aperture" size={50} color="white" />
             </TouchableOpacity>
             {videoUri ? (
               <TouchableOpacity className="items-end justify-end" onPress={saveVideo}>
@@ -94,5 +134,7 @@ export default function App() {
           </View>
         </View>
       </CameraView>
+      }
+    </View>
   );
 }
